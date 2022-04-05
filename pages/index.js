f4aa08b18d5fdabbe8c28c3dab6aef3232b26e96
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-05 11:23:43 +0530

    map

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab'
 import FilterListIcon from '@mui/icons-material/FilterList';
-import { Button } from "@mui/material";
+import { Button, Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 export default function Home() {
   const [ride, setRide] = useState('nearest');
@@ -20,27 +20,38 @@ export default function Home() {
   }
   return (
     <Layout title="Nearest Ride">
-      <div>
-        <Tabs
-          value={ride}
-          // textColor={rideType == ride ? "#FFF" : "#D0CBCB"}
-          onChange={handleChange}
-          aria-label="rideTypes"
-          TabIndicatorProps={{ style: { background: "#fff" } }}
-          style={{marginTop: "22px"}}
-          textColor="inherit"
-        >
-          <Tab label="Nearest rides" value="nearest"  className="rideOptions"/>
-          <Tab label="Upcoming rides (5)" value="upcoming" className="rideOptions" style={{marginLeft: "1rem"}} />
-          <Tab label="Past rides (4)" value="past" className="rideOptions" style={{marginLeft: "1rem"}}/>
-          <div className="grow"></div>
-          <Button className="filterButton" color="inherit">
-            <FilterListIcon /> Filters
-          </Button>
-          
-        </Tabs>
+      <Tabs
+        value={ride}
+        // textColor={rideType == ride ? "#FFF" : "#D0CBCB"}
+        onChange={handleChange}
+        aria-label="rideTypes"
+        TabIndicatorProps={{ style: { background: "#fff" } }}
+        style={{marginTop: "22px"}}
+        textColor="inherit"
+      >
+        <Tab label="Nearest rides" value="nearest"  className="rideOptions"/>
+        <Tab label="Upcoming rides (5)" value="upcoming" className="rideOptions" style={{marginLeft: "1rem"}} />
+        <Tab label="Past rides (4)" value="past" className="rideOptions" style={{marginLeft: "1rem"}}/>
+        <div className="grow"></div>
+        <Button className="filterButton" color="inherit">
+          <FilterListIcon /> Filters
+        </Button>
         
-      </div>
+      </Tabs>
+        
+      <Card className="rideCards" style={{backgroundColor: "#101010"}}>
+        <CardContent >
+          <img src="https://picsum.photos/200" alt="map" height="148" width="296" className="imageCard" />
+        </CardContent>
+        
+        <CardContent>
+          <Typography variant="body2" >
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
+          </Typography>
+        </CardContent>
+      </Card>
       
       
     </Layout>
