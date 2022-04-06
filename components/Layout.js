e9b4a638ffa6e1c7254c3f5199fbb1c6fe8883d2
GraniteMask <ratnadeepastronomy@gmail.com>
2022-04-06 19:31:17 +0530

    env added

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import NextLink from 'next/link'
-import {AppBar, Typography, Toolbar, Container, Link, ThemeProvider, CssBaseline, Switch, Badge, Button, Menu, MenuItem, Box, IconButton, Drawer, Divider, List, ListItem, ListItemText, makeStyles} from '@material-ui/core'
+import {AppBar, Typography, Toolbar, Link, ThemeProvider, CssBaseline,  Box, } from '@material-ui/core'
 import { createTheme } from '@material-ui/core/styles'
 import { Avatar } from '@mui/material'
 ;
